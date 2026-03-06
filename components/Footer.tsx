d779e932afe0ab45e90dfa6ca8618e123085bef5
SafeBuckleDev <69005869+SafeBuckleDev@users.noreply.github.com>
2026-03-06 14:36:23 +0100

    Added footer

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer({}) {
+  return (
+    <footer className="w-full bg-beige-text px-2 md:px-12 py-6 text-center text-beige-background text-xs">
+      <p>
+        © All rights to Genshin Impact game assets used on the website are
+        reserved by miHoYo Ltd. and Cognosphere Pte., Ltd. Other properties
+        belong to their respective owners. Project made by SafeBuckleDev
+      </p>
+    </footer>
+  );
+}
